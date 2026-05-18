27f7ca36fbb0cc55985768f84d488e3f5711ecc0
Vedika Bane <vedikaybane@gmail.com>
2026-05-18 22:23:38 +0530

    Update frontend API URL for production

--- client/src/services/api.ts
+++ client/src/services/api.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-    baseURL: "http://localhost:5000/api",
+    baseURL: "https://smart-leads-dashboard-779j.onrender.com/api",
 });
 
 api.interceptors.request.use((config) => {
